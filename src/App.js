6b4cad356ac91cef68bf0373898eeaa2b48d910e
angsabrina <angsabrina98@yahoo.com>
2021-09-27 22:44:32 -0700

    add data sample js and start call code

--- src/App.js
+++ src/App.js
@@ -1,22 +1,44 @@
+import React, { useState, useEffect } from "react";
 import logo from './logo.svg';
+// import { zillowSampleData } from "./data_sample";
 import './App.css';
 
 function App() {
+  const [zillowData, setZillowData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetch(
+      `https://api.bridgedataoutput.com/api/v2/zestimates_v2/zestimates?access_token=[token here]&address="98109"`,
+      {
+        method: "GET",
+        headers: new Headers({
+          Accept: "application/vnd.github.cloak-preview"
+        })
+      }
+    )
+      .then(res => res.json())
+      .then(response => {
+        console.log(response)
+        console.log(response.bundle)
+        setZillowData(response.bundle);
+        setIsLoading(false);
+      })
+      .catch(error => console.log(error))
+  });
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1> API calls with React Hooks </h1>
+
+        {isLoading && <p>Wait I'm loading zillow data for you</p>}
+
+        <ul>
+        {zillowData.map(item => <li key={item.address}>{item.address}</li>)}
+        </ul>
+
       </header>
     </div>
   );
